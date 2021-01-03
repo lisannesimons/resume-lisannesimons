56037c3182ddca01a7082559f64faa8ac6ce1bd1
Lisanne <hello@lisannesimons.eu>
2021-01-03 15:26:51 +0100

    Moved SMTP credentials to separate config file

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,8 +1,5 @@
 ﻿<?php
-
-// Replace this with your own email address
-$siteOwnersEmail = 'hello@lisannesimons.eu';
-
+require_once "config.php";
 require_once "Mail.php";
 
 if($_POST) {
@@ -40,15 +37,15 @@ if($_POST) {
 
     // Email Headers
     $headers['From'] = $from;
-    $headers['To'] = "hello@lisannesimons.eu";
+    $headers['To'] = $siteOwnersEmail;
+    $headers['Subject'] = $subject;
     $headers['Reply-To'] = $email;
     $headers['MIME-Version'] = "1.0";
     $headers['Content-Type'] = "text/html; charset=ISO-8859-1";
 
-
     if (!isset($error)) {
-	$smtp = Mail::factory('smtp', array ('host' => "tls://dennispotter.eu", 'port' => "465", 'auth' => true, 'username' => "hello@lisannesimons.eu", 'password' => "")); // Add password
-	$mail = $smtp->send("hello@lisannesimons.eu", $headers, $message);
+	$smtp = Mail::factory('smtp', array ('host' => $smtpServer, 'port' => $smtpPort, 'auth' => true, 'username' => $smtpEmail, 'password' => $smtpPassword));
+	$mail = $smtp->send($siteOwnersEmail, $headers, $message);
 	
 	if (PEAR::isError($mail)) {
 		// Disable error messaging. This could reveal bugs in the server
